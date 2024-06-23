ff4dd9169d6cd1ee483f44ef77280c6203c01a3c
Pakeeza Saeed <168451760+Pakeezasaeed1@users.noreply.github.com>
2024-06-23 12:01:00 +0200

    Update main.py
    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total bill? $"))
 tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
 tip = total_bill+(total_bill*tip_percentage/100)
-person_count = int(input("How many people to split the bill?"))
+person_count = int(input("How many people to split the bill??\n"))
 tip_person = tip/person_count
 final_amount = ("{:.2f}".format(round(tip_person, 2))) # "{:.2f}" used for 2 decimal places
 print(f"Each person should pay: ${final_amount}")
